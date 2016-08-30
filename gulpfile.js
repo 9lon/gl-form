7709149214371cb11888183d0a75218db3c384a8
Kitz-Parnupat <dainokung2535@gmail.com>
2016-08-30 17:09:41 +0700

    add gl-form-panel

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var replace = require('gulp-replace');
 
 gulp.task('htmlReplace',function(){
      gulp.src('src/gl-form-label.html')
+     gulp.src('src/gl-form-panel.html')
     .pipe(replace('../bower_components/','../../'))
     .pipe(gulp.dest(''));
 });
 
 gulp.task('watch',function(){
     gulp.watch('src/gl-form-label.html',['htmlReplace']);
+    gulp.watch('src/gl-form-panel.html',['htmlReplace']);
 });
 
 gulp.task('default', ['htmlReplace','watch']);
