4dfc63ab651c82de33a0cb788d8dcf9f9a89914b
Kitz-Parnupat <dainokung2535@gmail.com>
2016-08-30 17:49:01 +0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var replace = require('gulp-replace');
 gulp.task('htmlReplace',function(){
      gulp.src('src/gl-form-label.html')
      gulp.src('src/gl-form-panel.html')
-    .pipe(replace('../bower_components/','../../'))
+    .pipe(replace('../bower_components/','../'))
     .pipe(gulp.dest(''));
 });
 
